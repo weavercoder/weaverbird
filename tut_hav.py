e6a129a11d406337701d61717280ceeaeecd33bd
edwin njeru <weaverbirdcreed@gmail.com>
2023-06-26 14:17:03 -0400

    changes

---
+++ tut_hav.py
@@ -0,0 +1,8 @@
+#Ask user for their name
+name = input("whats your name? ").strip().title()
+#remove whitespase from str and captalize users name :- you can either use captalize for single name or title for both names
+#name = name.strip().title()
+#say hello to the user
+print(f"hello, {name} ")
+
+    
