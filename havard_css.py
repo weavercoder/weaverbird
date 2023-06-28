c007bcd3ebcb2664456a5593de46faa408403298
edwin njeru <weaverbirdcreed@gmail.com>
2023-06-28 13:55:42 -0400

    changes

--- havard_css.py
+++ havard_css.py
@@ -1,7 +1,11 @@
 #ask the user to enter name
 name = input("whats your name  :").strip().title()
 print(f"hallo,{name}")
-marital_status = input("are you married")
+marital_status = input("are you married :,bool = yes")
+if bool = yes
+    print(f"enter your spouse p_no  :")
+elif print("enter your own p_no :")
+    print("enter your own p_no  :")
 #if input("are you married, + bool = true")
 #print("enter your spouse p_no :")
 
